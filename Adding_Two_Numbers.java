ce11e101883c2188514cd8a62946deb965621496
saisuryavinay <147791886+saisuryavinay@users.noreply.github.com>
2024-08-06 11:12:30 +0530

    import java.util.*; public class codemind {     public static void main(String[] args)     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int m=s.nextInt();         int t=m+n;         System.out.println(t);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class codemind
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int m=s.nextInt();
+        int t=m+n;
+        System.out.println(t);
+    }
+}
